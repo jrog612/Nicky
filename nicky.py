ee405bdff59bd538d2d6bf8002666ba55aeaf808
joyongjin <wnrhd114@gmail.com>
2018-11-01 17:39:34 +0900

    nicky logic

---
+++ nicky.py
@@ -0,0 +1,28 @@
+import os
+
+SOURCE_PATH = 'source'
+
+
+class LoadMixin:
+    def load_prefix(self, lang='ko'):
+        lang_path = self.get_lang_path(lang)
+        prefix_path = os.path.join(lang_path, 'prefix')
+
+    def get_lang_path(self, lang='ko'):
+        lang_path = os.path.join(SOURCE_PATH, lang)
+        if not os.path.isdir(lang_path):
+            raise ValueError('Invalid language.')
+
+        return lang_path
+
+    def load_suffix(self, lang='ko'):
+        lang_path = self.get_lang_path(lang)
+        suffix_path = os.path.join(lang_path, 'suffix')
+
+
+class Nicky:
+    def __init__(self, lang='ko'):
+        self.lang = lang
+
+    def get_nickname(self, count=1):
+        pass
