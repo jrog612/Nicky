7075be974201f565541c467455c00261354dd31f
joyongjin <wnrhd114@gmail.com>
2018-12-18 11:17:13 +0900

    upload to pypi

--- README.rst
+++ README.rst
@@ -1,4 +1,8 @@
 Nicky
 -------------
+.. image:: https://badge.fury.io/py/nicky.svg
+    :target: https://badge.fury.io/py/nicky
 
-Nicky is the nicknamer.
+
+Nicky is the nicknamer. You can make funny nickname with nicky!
+
---
+++ setup.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import Command, find_packages, setup
+
+# Package meta-data.
+NAME = 'nicky'
+DESCRIPTION = 'Nicky the nicknamer.'
+URL = 'https://github.com/joyongjin/nicky'
+EMAIL = 'wnrhd114@gmail.com'
+AUTHOR = 'Jo Yongjin'
+REQUIRES_PYTHON = '>=3.5.3'
+VERSION = '0.0.1'
+
+REQUIRED = [
+    'click'
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    with io.open(os.path.join(here, 'README.rst')) as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+        sys.exit()
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    entry_points={
+        'console_scripts': ['nicky=nicky.nicky:cli'],
+    },
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    cmdclass={
+        'upload': UploadCommand,
+
+    }
+)
